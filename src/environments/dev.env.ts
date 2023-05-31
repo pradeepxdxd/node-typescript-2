802521b2c80dfaaa9ececc747e1979e0dc764ec7
pradeepxdxd <pradeepbiswas41813@gmail.com>
2023-05-31 14:27:14 +0530

    removing credentials

--- src/environments/dev.env.ts
+++ src/environments/dev.env.ts
@@ -1,9 +1,6 @@
 import { Environment } from "./env";
 
 export const DevEnvironment : Environment = {
-    db_url: "mongodb+srv://pradeep:pradeep@cluster0.dypu8yw.mongodb.net/?retryWrites=true&w=majority",
-    jwt_secret: "Secret",
-    email: "beastfake8@gmail.com",
-    password: "fffdczwtceefjocd",
+    // this credentials has removed because of security purpose
     port: 8000
 }
