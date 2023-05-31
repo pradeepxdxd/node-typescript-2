ac81be037276d4ab97a01407c927974e731cac1b
pradeepxdxd <pradeepbiswas41813@gmail.com>
2023-05-31 14:28:00 +0530

    removing credentials

--- src/environments/dev.env.ts
+++ src/environments/dev.env.ts
@@ -2,5 +2,9 @@ import { Environment } from "./env";
 
 export const DevEnvironment : Environment = {
     // this credentials has removed because of security purpose
-    port: 8000
+    port: 8000,
+    db_url: "",
+    jwt_secret: "",
+    email: "",
+    password: ""
 }
