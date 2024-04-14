06768dd630a6673df32f42d011109376b7bb660e
Pradeep Biswas <84695657+pradeepxdxd@users.noreply.github.com>
2024-04-14 12:08:39 +0530

    Update prod.env.ts

--- src/environments/prod.env.ts
+++ src/environments/prod.env.ts
@@ -4,6 +4,6 @@ export const ProdEnvironment : Environment = {
     db_url: "mongodb+srv://pradeep:pradeep@cluster0.dypu8yw.mongodb.net/?retryWrites=true&w=majority",
     jwt_secret: "ProdSecret",
     email: "beastfake8@gmail.com",
-    password: "fffdczwtceefjocd",
+    password: "",
     port: 8000
 }
